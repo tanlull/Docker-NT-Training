17d5f9a005f570dff49a27c7f3d41f4b18027b16
Tanya s <tanlull@gmail.com>
2022-05-19 11:09:35 +0700

    vue

---
+++ vue-modern-resume/src/views/dark-template/sidebar/Container.vue
@@ -0,0 +1,207 @@
+<template>
+  <v-card
+    color="grey darken-3"
+    dark
+  >
+    <v-card-text>
+      <avatar />
+      <div class="text-sm-center mb-4 mt-3">
+        <h1>
+          Tanya <span class="light-blue--text text--lighten-3">Sattayaaphitan</span>
+        </h1>
+        <span>
+          A challenge-loving Data Analyst
+        </span>
+      </div>
+
+      <sidebar-section :options="sections.info" />
+      <sidebar-section :options="sections.socials" />
+      <sidebar-section :options="sections.hobbies">
+        <template v-slot:item="{item}">
+          <v-chip>
+            <v-avatar>
+              <v-icon>
+                {{ item.icon }}
+              </v-icon>
+            </v-avatar>
+            {{ item.text }}
+          </v-chip>
+        </template>
+      </sidebar-section>
+      <sidebar-section :options="sections.languages">
+        <template v-slot:items="{items}">
+          <v-container pa-0>
+            <v-layout
+              wrap
+              class="text-xs-center"
+            >
+              <template
+                v-for="(item, i) in items"
+              >
+                <v-flex
+                  :key="i"
+                  md3
+                  sm4
+                  xs6
+                >
+                  <v-progress-circular
+                    rotate="360"
+                    size="65"
+                    width="2"
+                    :value="item.value"
+                    color="white"
+                    class="ma-2"
+                  >
+                    {{ item.text }}
+                  </v-progress-circular>
+                </v-flex>
+              </template>
+            </v-layout>
+          </v-container>
+        </template>
+      </sidebar-section>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import Avatar from '@/views/dark-template/sidebar/Avatar'
+import SidebarSection from '@/views/dark-template/sidebar/Section'
+export default {
+  name      : 'Sidebar',
+  components: { SidebarSection, Avatar },
+  data () {
+    return {
+      sections: {
+        info: {
+          title: 'INFO',
+          items: [
+            {
+              name: 'Email',
+              icon: 'mdi-email',
+              text: 'tanlull@gmail.com',
+            },
+            {
+              name: 'Website',
+              icon: 'mdi-web',
+              text: 'drtan.cc',
+            },
+            {
+              name: 'Birth Date',
+              icon: 'mdi-cake-variant',
+              text: 'Jan 11, 1975',
+            },
+            {
+              name: 'Habitation',
+              icon: 'mdi-map-marker',
+              text: 'Bangkok, Thailand',
+            },
+          ],
+        },
+        socials: {
+          title: 'SOCIALS',
+          items: [
+            {
+              icon: 'mdi-github-circle',
+              text: 'github.com/tanlull',
+              link: 'https://github.com/tanlull',
+            },
+            {
+              icon: 'mdi-linkedin-box',
+              text: 'linkedin.com/in/tanlull',
+              link: 'https://linkedin.com/in/tanlull',
+            },
+            {
+              icon: 'mdi-twitter',
+              text: 'twitter.com/tanlull',
+              link: 'https://twitter.com/tanlull',
+            },
+            {
+              icon: 'mdi-instagram',
+              text: 'instagram.com/tanlull',
+              link: 'https://instagram.com/tanlull',
+            },
+          ],
+        },
+        hobbies: {
+          title: 'HOBBIES',
+          items: [
+
+            {
+              icon: 'mdi-bullseye',
+              text: 'Getting out of Safe Zone',
+            },
+            {
+              icon: 'mdi-biohazard',
+              text: 'Challenges',
+            },
+            {
+              icon: 'mdi-bike',
+              text: 'Cycling',
+            },
+            {
+              icon: 'mdi-image-filter-hdr',
+              text: 'Nature',
+            },
+            {
+              icon: 'mdi-auto-fix',
+              text: 'Hacking Stuffs',
+            },
+            {
+              icon: 'mdi-teach',
+              text: 'Teaching',
+            },
+            {
+              icon: 'mdi-karate',
+              text: 'Sports',
+            },
+            {
+              icon: 'mdi-music',
+              text: 'Music',
+            },
+            {
+              icon: 'mdi-account-group',
+              text: 'Leadership',
+            },
+            {
+              icon: 'mdi-book-open-page-variant',
+              text: 'Books',
+            },
+            {
+              icon: 'mdi-android-debug-bridge',
+              text: 'Tickling Bugs!',
+            },
+          ],
+        },
+        languages: {
+          title: 'LANGUAGES',
+          items: [
+            {
+              text : 'English',
+              value: 70,
+            },
+            {
+              text : 'Turkish',
+              value: 45,
+            },
+            {
+              text : 'Azeri',
+              value: 100,
+            },
+            {
+              text : 'Persian',
+              value: 100,
+            },
+          ],
+        },
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+.sidebar{
+    background: #2e2e2e;
+}
+</style>
